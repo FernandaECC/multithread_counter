591458207b8ec935b6bc14cf08479de7cf3560e8
FenandaECC <f215835@dac.unicamp.br>
2020-07-01 21:27:53 -0300

    Sem comentários, a princípio funcionando.

--- src/main.c
+++ src/main.c
@@ -26,26 +26,33 @@ unsigned int primo(unsigned int N) {
 int n_completos = 0; //indica quantos numeros foram processados
 pthread_mutex_t trava;
 int count = 0; //indica quantos numeros primos tem na entrada
-
+int aux = 0;
+int pointer = 0;//apontador que ira percorrer o vetor que armazena valores de entrada
+int p=0, q=0; //variaveis utilizadas nos for's
+int array[100]; //vetor da entrada
 
 void* worker(void *arg) {
   int *N = (int*)(arg);
   int M = (*N);
-  printf("Iniciando thread %d\n", M);
+  //printf("Iniciando thread %d\n", M);
   int atuador = -1;
   
   
   while (1) {
     if (n_completos >= p) break;
     pthread_mutex_lock(&trava);
-    atuador = n_completos;
+    atuador = array[pointer];
     n_completos += 1;
+    aux = primo(array[pointer]);
+    if (aux == 1) {(count)++;}
+    pointer++;
     pthread_mutex_unlock(&trava);
-    printf("Numero %d acessado por thread %d\n", atuador, M);
-    numeros[atuador] *= 2;
+    //printf("Numero %d acessado por thread %d\n", atuador, M);
+    
+    
     for (int j=0; j<500000; j++);
   }
-  printf("Saindo de thread %d\n", M);
+  //printf("Saindo de thread %d\n", M);
   return NULL;
 }
 
@@ -58,33 +65,41 @@ int main(int argc, char **argv) {
 
 
   /*Tratamento e armazenamento da sequência de números (input)*/
-  int p=0, q=0; //variaveis utilizadas nos for's
-  
-  int array[100]; //vetor da entrada
+
   char temp;
   do {
       scanf("%d%c", &array[p], &temp);
       p++;
       } while(temp != '\n');
 
+int num_threads = 0;
 
+  // definindo o número de threads
+  if(p <= 4){
+   num_threads = (p % 5);
+  }
+  
+  else if(p > 4){
+  num_threads = THREADS_MAX;
+  }
 
+//printf("numero de threads: %d\n", num_threads);
   /* Identificadores de thread */
-  for (int i=0; i<THREADS_MAX; i++) {
+  for (int i=0; i<num_threads; i++) {
     thread_id[i] = i;
   }
 
   /* Disparanto threads */
-  for (int i=0; i<THREADS_MAX; i++) {
+  for (int i=0; i<num_threads; i++) {
     pthread_create(&(workers[i]), NULL, worker, (void*) (&thread_id[i]));
   }
 
   /* Esperando threads */
-  for (int i=0; i<THREADS_MAX; i++) {
+  for (int i=0; i<num_threads; i++) {
     pthread_join(workers[i], NULL);
   }
 
-
+printf("%d\n", count);
 
   return 0;
 }
